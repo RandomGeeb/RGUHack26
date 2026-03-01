ed3757c0356e42916b56d8b494fab80360f8551a
Harry Tennant <c5030779@newcastle.ac.uk>
2026-03-01 07:48:11 +0000

    s

--- escape-employment/Assets/Scripts/ItemData.cs
+++ escape-employment/Assets/Scripts/ItemData.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public enum ItemType { Noise, Decoy }
+public enum ItemType { Noise, Decoy, Key }
 
 [CreateAssetMenu(fileName = "NewItem", menuName = "Stealth/Item")]
 public class ItemData : ScriptableObject
